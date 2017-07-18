38df038e99d4ec375206d1fdf3b30709d5b43bbe
Athina Bekakou <athinabek@grnet.gr>
2017-07-18 19:02:29 +0300

    Fix whitespace and indentation
    - Convert tabs to spaces
    - Remove trailing spaces
    - Fix indentation

--- themes/ssp/default/includes/footer.php
+++ themes/ssp/default/includes/footer.php
@@ -1,19 +1,19 @@
 <?php
 if(!empty($this->data['htmlinject']['htmlContentPost'])) {
-	foreach($this->data['htmlinject']['htmlContentPost'] AS $c) {
-		echo $c;
-	}
+  foreach($this->data['htmlinject']['htmlContentPost'] AS $c) {
+    echo $c;
+  }
 }
 ?>
-	</div><!-- #content -->
-	<div id="footer">
-		<hr />
+  </div><!-- #content -->
+  <div id="footer">
+    <hr />
 
-		Copyright &copy; 2016 <a href="https://openminted.eu/">OpenMinTeD</a> | Powered by <a href="https://github.com/rciam">RCIAM</a>
-		
-		<br style="clear: right" /><br/>
-	
-	</div><!-- #footer -->
+    Copyright &copy; 2016 <a href="https://openminted.eu/">OpenMinTeD</a> | Powered by <a href="https://github.com/rciam">RCIAM</a>
+
+    <br style="clear: right" /><br/>
+
+  </div><!-- #footer -->
 
 </div><!-- #wrap -->
 
