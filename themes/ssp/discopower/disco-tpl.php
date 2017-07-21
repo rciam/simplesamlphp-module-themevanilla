ab990259a7e8ed53fe69bd084afa5ddd3c34d4c6
Athina Bekakou <athinabek@grnet.gr>
2017-07-21 11:19:15 +0300

    Put providers in panels

--- themes/ssp/discopower/disco-tpl.php
+++ themes/ssp/discopower/disco-tpl.php
@@ -1,15 +1,10 @@
 <?php
 
-
 $faventry = NULL;
 foreach( $this->data['idplist'] AS $tab => $slist) {
   if (!empty($this->data['preferredidp']) && array_key_exists($this->data['preferredidp'], $slist))
     $faventry = $slist[$this->data['preferredidp']];
 }
-
-
-
-
 
 
 if(!array_key_exists('header', $this->data)) {
@@ -18,15 +13,14 @@ if(!array_key_exists('header', $this->data)) {
 $this->data['header'] = $this->t($this->data['header']);
 $this->data['jquery'] = array('core' => TRUE, 'ui' => TRUE, 'css' => TRUE);
 
-$this->data['head'] = '<link rel="stylesheet" media="screen" type="text/css" href="' . SimpleSAML_Module::getModuleUrl('discopower/style.css')  . '" />';
+// $this->data['head'] = '<link rel="stylesheet" media="screen" type="text/css" href="' . SimpleSAML_Module::getModuleUrl('discopower/style.css')  . '" />';
 
 $this->data['head'] .= '<script type="text/javascript" src="' . SimpleSAML_Module::getModuleUrl('discopower/js/jquery.livesearch.js')  . '"></script>';
 $this->data['head'] .= '<script type="text/javascript" src="' . SimpleSAML_Module::getModuleUrl('discopower/js/' . $this->data['score'] . '.js')  . '"></script>';
 
 $this->data['head'] .= '<script type="text/javascript">
 
-  $(document).ready(function() {
-    $("#discotabs").tabs({ selected: ' . $this->data['defaulttab'] . ' }); ';
+  $(document).ready(function() {';
 
     $i = 0;
     foreach ($this->data['idplist'] AS $tab => $slist) {
@@ -64,7 +58,7 @@ function showEntry($t, $metadata, $favourite = FALSE) {
 
   if(array_key_exists('icon', $metadata) && $metadata['icon'] !== NULL) {
     $iconUrl = \SimpleSAML\Utils\HTTP::resolveURL($metadata['icon']);
-    $html .= '<img alt="Icon for identity provider" class="entryicon" src="' . htmlspecialchars($iconUrl) . '" />';
+    $html .= '<img alt="Identity Provider" class="entryicon" src="' . htmlspecialchars($iconUrl) . '" />';
   }
 
   $html .= '</a>';
@@ -104,76 +98,59 @@ function getTranslatedName($t, $metadata) {
 if (!empty($faventry)) {
 
 
-  echo('<div class="favourite">');
+  echo('<div class="panel panel-default favourite">');
   echo($this->t('previous_auth'));
   echo(' <strong>' . htmlspecialchars(getTranslatedName($this, $faventry)) . '</strong>');
   echo('
-  <form id="idpselectform" method="get" action="' . $this->data['urlpattern'] . '">
+  <form id="idpselectform" method="get" action="' . $this->data['urlpattern'] . '" class="b-form-favourite">
     <input type="hidden" name="entityID" value="' . htmlspecialchars($this->data['entityID']) . '" />
     <input type="hidden" name="return" value="' . htmlspecialchars($this->data['return']) . '" />
     <input type="hidden" name="returnIDParam" value="' . htmlspecialchars($this->data['returnIDParam']) . '" />
     <input type="hidden" name="idpentityid" value="' . htmlspecialchars($faventry['entityid']) . '" />
 
-    <input type="submit" name="formsubmit" id="favouritesubmit" value="' . $this->t('login_at') . ' ' . htmlspecialchars(getTranslatedName($this, $faventry)) . '" />
+    <input type="submit" name="formsubmit" id="favouritesubmit" class="btn btn-default" value="' . $this->t('login_at') . ' ' . htmlspecialchars(getTranslatedName($this, $faventry)) . '" />
   </form>');
 
-  echo('</div>');
+  echo('</div>'); // /panel
 }
 
 
 ?>
-
-
-
-
-
-
-<div id="discotabs">
-
-    <ul class="tabset_tabs">
-      <?php
-
-        $tabs = array_keys( $this->data['idplist']);
-        foreach ($tabs AS $tab) {
-      if(!empty($this->data['idplist'][$tab])) {
-        echo '<li><a href="#' . $tab . '"><span>' . $this->t('{discopower:tabs:' . $tab . '}') . '</span></a></li> ';
-      }
-        }
-
-      ?>
-    </ul>
-
 
 <?php
 
-
-
-
 foreach( $this->data['idplist'] AS $tab => $slist) {
+  if ($tab !== 'all') {
+    echo '<div class="panel panel-default b-panel-providers">
+            <div class="panel=heading">
+              <h3 class="panel-title">' . $this->t('{discopower:tabs:' . $tab . '}') . '</h3>' .
+            '</div>';
+    if (!empty($slist)) {
 
-  echo '<div id="' . $tab . '">';
+      echo('	<div class="inlinesearch">');
+      echo('	<p>Incremental search...</p>');
+      echo('	<form id="idpselectform" action="?" method="get"><input class="inlinesearchf" type="text" value="" name="query_' . $tab . '" id="query_' . $tab . '" /></form>');
+      echo('	</div>');
 
-  if (!empty($slist)) {
-
-    echo('	<div class="inlinesearch">');
-    echo('	<p>Incremental search...</p>');
-    echo('	<form id="idpselectform" action="?" method="get"><input class="inlinesearchf" type="text" value="" name="query_' . $tab . '" id="query_' . $tab . '" /></form>');
-    echo('	</div>');
-
-    echo('	<div class="metalist" id="list_' . $tab  . '">');
-    if (!empty($this->data['preferredidp']) && array_key_exists($this->data['preferredidp'], $slist)) {
-      $idpentry = $slist[$this->data['preferredidp']];
-      echo (showEntry($this, $idpentry, TRUE));
-    }
-
-    foreach ($slist AS $idpentry) {
-      if ($idpentry['entityid'] != $this->data['preferredidp']) {
-        echo (showEntry($this, $idpentry));
+      echo('	<ul class="metalist b-panel-providers__list" id="list_' . $tab  . '">');
+      if (!empty($this->data['preferredidp']) && array_key_exists($this->data['preferredidp'], $slist)) {
+        $idpentry = $slist[$this->data['preferredidp']];
+        echo '<li style="background:yellow;">';
+        echo (showEntry($this, $idpentry, TRUE));
+        echo '</li>';
       }
+
+      foreach ($slist AS $idpentry) {
+        if ($idpentry['entityid'] != $this->data['preferredidp']) {
+        echo '<li style="background:beige;">';
+          echo (showEntry($this, $idpentry));
+        echo '</li>';
+        }
+      }
+      echo('	</ul>'); // /metalist
     }
-    echo('	</div>');
   }
-  echo '</div>';
+  echo '</div>'; // /panel
 
 }
 
@@ -181,7 +158,6 @@ foreach( $this->data['idplist'] AS $tab => $slist) {
 
 
 
-</div>
 
 
 <?php $this->includeAtTemplateBase('includes/footer.php');
