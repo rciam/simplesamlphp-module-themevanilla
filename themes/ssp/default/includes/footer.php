cbb8671a96c3e4ffab4dbac04dbddfdfb26e7ec7
Athina Bekakou <athinabek@grnet.gr>
2017-08-02 15:15:52 +0300

    Fix indentation

--- themes/ssp/default/includes/footer.php
+++ themes/ssp/default/includes/footer.php
@@ -12,78 +12,78 @@ if(!empty($this->data['htmlinject']['htmlContentPost'])) {
 
 <?php
 
-    $includeLanguageBar = TRUE;
-    if (!empty($_POST))
-      $includeLanguageBar = FALSE;
-    if (isset($this->data['hideLanguageBar']) && $this->data['hideLanguageBar'] === TRUE)
-      $includeLanguageBar = FALSE;
+$includeLanguageBar = TRUE;
+if (!empty($_POST))
+  $includeLanguageBar = FALSE;
+if (isset($this->data['hideLanguageBar']) && $this->data['hideLanguageBar'] === TRUE)
+  $includeLanguageBar = FALSE;
 
-    if ($includeLanguageBar) {
+if ($includeLanguageBar) {
 
-      $languages = $this->getLanguageList();
-      if ( count($languages) > 1 ) {
-        echo '<div class="b-lang-container">
-                <div class="dropup">';
-        $langnames = array(
-          'no' => 'Bokmål', // Norwegian Bokmål
-          'nn' => 'Nynorsk', // Norwegian Nynorsk
-          'se' => 'Sámegiella', // Northern Sami
-          'sam' => 'Åarjelh-saemien giele', // Southern Sami
-          'da' => 'Dansk', // Danish
-          'en' => 'English',
-          'de' => 'Deutsch', // German
-          'sv' => 'Svenska', // Swedish
-          'fi' => 'Suomeksi', // Finnish
-          'es' => 'Español', // Spanish
-          'fr' => 'Français', // French
-          'it' => 'Italiano', // Italian
-          'nl' => 'Nederlands', // Dutch
-          'lb' => 'Lëtzebuergesch', // Luxembourgish
-          'cs' => 'Čeština', // Czech
-          'sl' => 'Slovenščina', // Slovensk
-          'lt' => 'Lietuvių kalba', // Lithuanian
-          'hr' => 'Hrvatski', // Croatian
-          'hu' => 'Magyar', // Hungarian
-          'pl' => 'Język polski', // Polish
-          'pt' => 'Português', // Portuguese
-          'pt-br' => 'Português brasileiro', // Portuguese
-          'ru' => 'русский язык', // Russian
-          'et' => 'eesti keel', // Estonian
-          'tr' => 'Türkçe', // Turkish
-          'el' => 'ελληνικά', // Greek
-          'ja' => '日本語', // Japanese
-          'zh' => '简体中文', // Chinese (simplified)
-          'zh-tw' => '繁體中文', // Chinese (traditional)
-          'ar' => 'العربية', // Arabic
-          'fa' => 'پارسی', // Persian
-          'ur' => 'اردو', // Urdu
-          'he' => 'עִבְרִית', // Hebrew
-          'id' => 'Bahasa Indonesia', // Indonesian
-          'sr' => 'Srpski', // Serbian
-          'lv' => 'Latviešu', // Latvian
-          'ro' => 'Românește', // Romanian
-          'eu' => 'Euskara', // Basque
-        );
+  $languages = $this->getLanguageList();
+  if ( count($languages) > 1 ) {
+    echo '<div class="b-lang-container">
+      <div class="dropup">';
+    $langnames = array(
+      'no' => 'Bokmål', // Norwegian Bokmål
+      'nn' => 'Nynorsk', // Norwegian Nynorsk
+      'se' => 'Sámegiella', // Northern Sami
+      'sam' => 'Åarjelh-saemien giele', // Southern Sami
+      'da' => 'Dansk', // Danish
+      'en' => 'English',
+      'de' => 'Deutsch', // German
+      'sv' => 'Svenska', // Swedish
+      'fi' => 'Suomeksi', // Finnish
+      'es' => 'Español', // Spanish
+      'fr' => 'Français', // French
+      'it' => 'Italiano', // Italian
+      'nl' => 'Nederlands', // Dutch
+      'lb' => 'Lëtzebuergesch', // Luxembourgish
+      'cs' => 'Čeština', // Czech
+      'sl' => 'Slovenščina', // Slovensk
+      'lt' => 'Lietuvių kalba', // Lithuanian
+      'hr' => 'Hrvatski', // Croatian
+      'hu' => 'Magyar', // Hungarian
+      'pl' => 'Język polski', // Polish
+      'pt' => 'Português', // Portuguese
+      'pt-br' => 'Português brasileiro', // Portuguese
+      'ru' => 'русский язык', // Russian
+      'et' => 'eesti keel', // Estonian
+      'tr' => 'Türkçe', // Turkish
+      'el' => 'ελληνικά', // Greek
+      'ja' => '日本語', // Japanese
+      'zh' => '简体中文', // Chinese (simplified)
+      'zh-tw' => '繁體中文', // Chinese (traditional)
+      'ar' => 'العربية', // Arabic
+      'fa' => 'پارسی', // Persian
+      'ur' => 'اردو', // Urdu
+      'he' => 'עִבְרִית', // Hebrew
+      'id' => 'Bahasa Indonesia', // Indonesian
+      'sr' => 'Srpski', // Serbian
+      'lv' => 'Latviešu', // Latvian
+      'ro' => 'Românește', // Romanian
+      'eu' => 'Euskara', // Basque
+    );
 
-        $textarray = array();
-        foreach ($languages AS $lang => $current) {
-          $lang = strtolower($lang);
-          if ($current) {
-            $lang_current = $langnames[$lang];
-          } else {
-            $textarray[] = '<li class="b-dropdown__two_cols--item"><a href="' . htmlspecialchars(\SimpleSAML\Utils\HTTP::addURLParameters(\SimpleSAML\Utils\HTTP::getSelfURL(), array($this->languageParameterName => $lang))) . '">' .
-              $langnames[$lang] . '</a></li>';
-          }
-        }
-        echo '<button class="b-btn btn b-btn__footer dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">'
-            . $lang_current
-            . '<span class="caret"></span>
-               </button>
-              <ul class="dropdown-menu dropdown-menu-left b-dropdown__two_cols" aria-labelledby="dropdownMenu1">';
-        echo join(' ', $textarray);
-        echo '</ul></div></div>'; // /dropup /b-lang-container
+    $textarray = array();
+    foreach ($languages AS $lang => $current) {
+      $lang = strtolower($lang);
+      if ($current) {
+        $lang_current = $langnames[$lang];
+      } else {
+        $textarray[] = '<li class="b-dropdown__two_cols--item"><a href="' . htmlspecialchars(\SimpleSAML\Utils\HTTP::addURLParameters(\SimpleSAML\Utils\HTTP::getSelfURL(), array($this->languageParameterName => $lang))) . '">' .
+          $langnames[$lang] . '</a></li>';
       }
     }
+    echo '<button class="b-btn btn b-btn__footer dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">'
+      . $lang_current
+      . '<span class="caret"></span>
+      </button>
+      <ul class="dropdown-menu dropdown-menu-left b-dropdown__two_cols" aria-labelledby="dropdownMenu1">';
+    echo join(' ', $textarray);
+    echo '</ul></div></div>'; // /dropup /b-lang-container
+  }
+}
 
 ?>
         <div class="copy">Copyright &copy; 2016-2017 <a href="https://openminted.eu/">OpenMinTeD</a></div>
