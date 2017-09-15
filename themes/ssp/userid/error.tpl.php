3ebf9edee372124cf0609a9057c1a509b97b2b12
Athina Bekakou <athinabek@grnet.gr>
2017-09-15 15:01:58 +0300

    userid: Add real data in template

--- themes/ssp/userid/error.tpl.php
+++ themes/ssp/userid/error.tpl.php
@@ -11,10 +11,13 @@ $this->includeAtTemplateBase('includes/header.php');
 ?>
 <div class="row">
   <div class="col-sm-12">
-    <h2><?php echo $this->t('{themeopenminted:userid_error:friendly_title}'); ?></h2>
+  <?php
+    $friendly_title = '<h2>' . $this->t('{themeopenminted:userid_error:friendly_title}') . ' ' . $this->data['parameters']['%IDP%'] . '</h2>';
+    echo $friendly_title;
+  ?>
     <p><?php echo $this->t('{themeopenminted:userid_error:friendly_description}'); ?></p>
     <div class="text-center">
-      <a href="/" class="ssp-btn btn ssp-btn__action text-uppercase">
+    <a href="<?php echo $this->data['parameters']['%BASEDIR%']; ?>saml2/idp/initSLO.php?RelayState=<?php echo urlencode($this->data['parameters']['%RESTARTURL%']); ?>" class="ssp-btn btn ssp-btn__action text-uppercase">
         <?php echo $this->t('{themeopenminted:userid_error:go2disco}'); ?>
       </a>
     </div>
@@ -25,10 +28,7 @@ $this->includeAtTemplateBase('includes/header.php');
     <h2><?php echo $this->t('{themeopenminted:userid_error:details_title}'); ?></h2>
     <p><?php echo $this->t('{themeopenminted:userid_error:details_description}'); ?></p>
     <pre class="ssp-error-code">
-      eduPersonUniqueId
-      eduPersonPrincipalName
-      eduPersonTargetedID
-      persistentNameId
+      <?php foreach ($this->data['parameters']['%ATTRIBUTES%'] as $attr) echo $attr . '<br>'; ?>
     </pre>
   </div>
 </div>
