1c46085af5c0f6b2b57b292d670bc8d63a91a9fc
Olga Brani <olgabrani@grnet.gr>
2018-09-28 14:16:11 +0300

    Add vanilla favicon

--- themes/ssp/default/includes/header.php
+++ themes/ssp/default/includes/header.php
@@ -39,8 +39,9 @@ if (array_key_exists('pageid', $this->data)) {
  */
 header('X-Frame-Options: SAMEORIGIN');
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+?>
+<!DOCTYPE html>
+<html lang="en-US">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0" />
@@ -51,7 +52,7 @@ header('X-Frame-Options: SAMEORIGIN');
 </title>
 
 <link rel="stylesheet" type="text/css" href="<?php echo htmlspecialchars(SimpleSAML_Module::getModuleURL('themevanilla/resources/css/app.css')); ?>" />
-<link rel="shortcut icon" href="<?php echo htmlspecialchars(SimpleSAML_Module::getModuleURL('themevanilla/resources/images/favicon.png')); ?>"  />
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo htmlspecialchars(SimpleSAML_Module::getModuleURL('themevanilla/resources/images/favicon.ico')); ?>"  />
 
 <?php
 
