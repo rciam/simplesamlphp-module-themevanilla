39b9c921928b8efd7781096ba1cec4d6ff6019bc
Olga Brani <olgabrani@grnet.gr>
2018-10-01 16:50:38 +0300

    Add X-UA-Compatible Meta Tag for IE

--- themes/ssp/default/includes/header.php
+++ themes/ssp/default/includes/header.php
@@ -44,6 +44,7 @@ header('X-Frame-Options: SAMEORIGIN');
 <html lang="en-US">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0" />
 <script type="text/javascript" src="/<?php echo $this->data['baseurlpath']; ?>resources/script.js"></script>
 <title>
