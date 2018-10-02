1609e5fd643fc18313ee48b2b2343d0560dad896
Nikolaos Evangelou <nikosev@grnet.gr>
2018-10-02 14:22:48 +0300

    Simplify policy.php

--- www/policy.php
+++ www/policy.php
@@ -1,17 +1,7 @@
 <?php
 // Load SimpleSAMLphp, configuration
 $config = SimpleSAML_Configuration::getInstance();
-$session = SimpleSAML_Session::getSessionFromRequest();
-
-// Check if valid local session exists.
-if ($config->getBoolean('admin.protectindexpage', false)) {
-    SimpleSAML\Utils\Auth::requireAdmin();
-}
-$loginurl = SimpleSAML\Utils\Auth::getAdminLoginURL();
-$isadmin = SimpleSAML\Utils\Auth::isAdmin();
 
 $t = new SimpleSAML_XHTML_Template($config, 'themevanilla:policy.tpl.php');
 $t->data['pageid'] = 'policy';
-$t->data['isadmin'] = $isadmin;
-$t->data['loginurl'] = $loginurl;
 $t->show();
