64664b135bf297231ee010ea5fba4aba42c67a33
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2023-12-07 12:22:07 +0200

    sspv2 theme vanilla

--- themes/ssp/default/includes/cmg_loader.php
+++
@@ -1,8 +0,0 @@
-<div id="cmg-loader" class="loader-container">
-  <div class="redirect-box">
-    <div class="lds-spinner">
-      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-    </div>
-    <span id="cmg-loader-comment" class="comment" style="display: none;">Please wait...</span>
-  </div>
-</div>
